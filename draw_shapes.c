7133d01199b678833035f2f2635263e924bbb773
Estrelliita <estrellalr216@gmail.com>
2024-02-06 17:35:31 -0700

    created print_arrow(leftCol, size), modified message for user

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,10 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+/* Print a size x size arrow ....*/
+void print_arrow(int leftCol, int size)
+{
+  print_triangle(leftCol, size);
+  print_square(leftCol+4, size);
+}
+
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Arrow = a, Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -16,9 +16,13 @@ int main()
 
     // Given the user answer, select which method to call
     switch (c) {
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5, 7);
+      break;
     case 't':
       puts("You selected triangle:");
-      print_arrow(5, 7);
+      print_triangle(5,7);
       break;
     case 's':
       puts("You selected square:");
